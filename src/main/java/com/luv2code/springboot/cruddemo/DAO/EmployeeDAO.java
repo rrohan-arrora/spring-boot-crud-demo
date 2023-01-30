23210b710e14368a401971bc525ca5714bea4894
Rohan Arora <mac@macs-MacBook-Air.local>
2023-01-30 12:25:19 +0530

    fetched all the employees using native hibernative API by creating session using entity manager

--- src/main/java/com/luv2code/springboot/cruddemo/DAO/EmployeeDAO.java
+++ src/main/java/com/luv2code/springboot/cruddemo/DAO/EmployeeDAO.java
@@ -1,5 +1,10 @@
 package com.luv2code.springboot.cruddemo.DAO;
 
-public class EmployeeDAO {
+import java.util.List;
 
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeDAO {
+	
+	public List<Employee> findAll();
 }
--- src/main/java/com/luv2code/springboot/cruddemo/DAO/EmployeeDAOImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/DAO/EmployeeDAOImpl.java
@@ -1,5 +1,38 @@
 package com.luv2code.springboot.cruddemo.DAO;
 
-public class EmployeeDAOImpl {
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+import jakarta.persistence.EntityManager;
+
+public class EmployeeDAOImpl implements EmployeeDAO{
+	
+	//define field for entity manager
+	@Autowired
+	private EntityManager entityManager;
+
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		//create a query
+		Query<Employee> theQuery = 
+				currentSession.createQuery("from Employee", Employee.class);
+		
+		//execute query and get the result list
+		List<Employee> employees = theQuery.getResultList();
+		
+		//return the results
+		return employees;
+	}
 
 }
