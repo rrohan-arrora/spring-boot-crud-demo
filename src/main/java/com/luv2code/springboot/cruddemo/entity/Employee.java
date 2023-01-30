0a2dec63d538812923c1a5186a48873f1a44e263
Rohan Arora <mac@macs-MacBook-Air.local>
2023-01-30 13:45:51 +0530

    Added code for the methods save

--- src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -18,10 +18,10 @@ public class Employee {
 	private int id;
 	
 	@Column(name="first_name")
-	private String first_name;
+	private String firstName;
 	
 	@Column(name="last_name")
-	private String last_name;
+	private String lastName;
 	
 	@Column(name="email")
 	private String email;
@@ -32,10 +32,10 @@ public class Employee {
 		
 	}
 	
-	public Employee(String first_name, String last_name, String email) {
+	public Employee(String firstName, String lastName, String email) {
 		super();
-		this.first_name = first_name;
-		this.last_name = last_name;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.email = email;
 	}
 
@@ -48,20 +48,20 @@ public class Employee {
 		this.id = id;
 	}
 
-	public String getFirst_name() {
-		return first_name;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
+	public void setFirstName(String first_name) {
+		this.firstName = first_name;
 	}
 
-	public String getLast_name() {
-		return last_name;
+	public String getLastName() {
+		return lastName;
 	}
 
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
+	public void setLastName(String last_name) {
+		this.lastName = last_name;
 	}
 
 	public String getEmail() {
@@ -75,7 +75,7 @@ public class Employee {
 	//define tostirng
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", first_name=" + first_name + ", last_name=" + last_name + ", email=" + email
+		return "Employee [id=" + id + ", first_name=" + firstName + ", last_name=" + lastName + ", email=" + email
 				+ "]";
 	}
 
--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luv2code.springboot.cruddemo.DAO.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 
@@ -27,5 +29,26 @@ public class EmployeeRestController {
 	@GetMapping("/employees")
 	public List<Employee> findAll(){
 		return eService.findAll();
+	}
+	
+	//expose "/employees/{employeeId}" and return list of employees
+	@GetMapping("/employees/{employeeId}")
+	public Employee findOne(@PathVariable int employeeId){
+		Employee theEmployee = eService.findOne(employeeId);
+		
+		if(theEmployee==null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		
+		return theEmployee;
+	}
+		
+	//expose "/employees/{theEmployeeId}" and return list of employees
+	@PostMapping("/employees")
+	public void save(@RequestBody Employee theEmployee ){
+		
+		// just in case, user pass an id in JSON ... set id to 0
+		// this is to force a save of new item ... instead of update
+		eService.save(theEmployee);
 	}
 }
